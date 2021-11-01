59f2c1e31a435ce54656407155b55d07e7295f0e
aji1904 <38060397+aji1904@users.noreply.github.com>
2021-11-01 07:07:38 +0700

    add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/index.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:page_transition/page_transition.dart';
 
-void main() => runApp(MaterialApp(
-    builder: (context, child) {
-      return Directionality(textDirection: TextDirection.ltr, child: child!);
-    },
-    title: 'APAM',
-    theme: ThemeData(
-      primaryColor: Colors.grey[800],
-    ),
-    home: const MainApp()));
+void main() => runApp(
+      MaterialApp(
+        builder: (context, child) {
+          return Directionality(
+              textDirection: TextDirection.ltr, child: child!);
+        },
+        title: 'APAM',
+        theme: ThemeData(
+          primaryColor: Colors.grey[800],
+        ),
+        home: AnimatedSplashScreen(
+          duration: 3000,
+          splash: "assets/images/logo.png",
+          nextScreen: const MainApp(),
+          splashTransition: SplashTransition.slideTransition,
+          pageTransitionType: PageTransitionType.topToBottom,
+          backgroundColor: const Color.fromRGBO(231, 247, 233, 1),
+        ),
+      ),
+    );
